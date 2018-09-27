fcb6c06f39c6fadb6c6d36315d23bdb4d619ae0c
mituki suzu <s.0x0.n_1209@ezweb.ne.jp>
2018-09-27 19:12:13 +0900

    change code. satoshi

--- common/js/main.js
+++ common/js/main.js
@@ -1,21 +1,57 @@
 $(document).ready(function(){
-	var slick = $('.single-item').slick({
-		appendArrows: $('#arrows'),
-		//左右ボタンで画像の切り替えができるかどうか
-		accessibility: true,
-		//自動再生するか
-		autoplay: true,
-		//自動再生の切り替え時間(ms)
-		autoplaySpeed: 2000,
-		// 自動再生や左右の矢印でスライドするスピード
-		speed: 3000,
-		//画像下のドットを表示
-		dots: true,
-		//切り替え時のフェードイン
-		fade: true,
-		//無限スクロールにするかどうか
-		infinite: true,
-		//マウスオーバー時スクロールを止めるかどうか
-		pauseOnHover: true,
+
+	var $section = $('.js-section'); // 各スライド
+	var $pager = $('#js-pager'); // ページャー枠
+
+	// ページャーの作成
+	function createPager() {
+		$section.each(function(i, e){
+			// ページ内リンク先の作成
+			var sectionName = $(e).attr('data-section-name');
+			// 最初のliにはクラスを付与
+			var addClass = '';
+			if (i === 0) {
+			    addClass = 'is-current';
+			}
+			// liのHTML作成
+			var html = '';
+			html += '<li class="' + addClass + '">';
+			html += '<a href="#' + sectionName + '"></a>';
+			html += '</li>';
+			$pager.append(html);
+		});
+
+		pagerLink();
+	}
+
+	// ページャーでaタグをクリックされたらスクロールする
+	function pagerLink () {
+		$pager.find('a').on('click', function() {
+			$.scrollify.move($(this).attr("href"));
+		});
+	}
+
+	// ページャーに対応する順番にクラス名を付与
+	function pagerCurrent(index = 0) {
+		var $li = $pager.find('li');
+		$li.removeClass('is-current');
+		$li.eq(index).addClass('is-current');
+	}
+
+	var option = {
+		section : '.js-section', // 対象を指定
+		easing: "swing", // イージングをしてい(jQueryのanimation)
+		scrollSpeed: 600, // スクロール時の速度
+		scrollbars: false, // スクロールバーを表示するか
+		before:function(index, section) {
+			pagerCurrent(index); // ページャーに対応する順番にクラス名を付与
+		},
+		afterRender:function() {
+			createPager(); // ページャーの作成
+		}
+	};
+
+	$(function() {
+		$.scrollify(option); // scrollifyの実行
 	});
 });
